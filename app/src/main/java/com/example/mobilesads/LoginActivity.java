bb2da1c7e3ad1cfe993d27732a51bc67984a564c
Bidooooooooooooo <ana_bedo>
2022-08-09 17:01:51 +0200

    s

--- app/src/main/java/com/example/mobilesads/LoginActivity.java
+++ app/src/main/java/com/example/mobilesads/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.mobilesads;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,12 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -16,12 +23,18 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText username,password;
     Button loginbtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth=FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser()!=null){
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        }
         username =findViewById(R.id.et_email);
         password=findViewById(R.id.et_pass);
         loginbtn=findViewById(R.id.btn_login);
@@ -44,9 +57,21 @@ public class LoginActivity extends AppCompatActivity {
                     password.setError("Must be greater than 8 elements..");
                 }
                 else{
-                    Intent intent=new Intent(LoginActivity.this,MainActivity.class);
-                    intent.putExtra("user",user);
-                    startActivity(intent);
+                    mAuth.createUserWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+
+                                Toast.makeText(getApplicationContext(),"Successful",Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+                            }else {
+                                Toast.makeText(getApplicationContext(),"Problem",Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
                 }
 
             }
--- app/src/main/java/com/example/mobilesads/MainActivity.java
+++ app/src/main/java/com/example/mobilesads/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mobilesads;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
@@ -10,16 +11,23 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+
 import com.example.mobilesads.adapters.Adapter;
 import com.example.mobilesads.adapters.OnProductClickListener;
 import com.example.mobilesads.model.Products;
 import com.example.mobilesads.viewmodel.ProductsViewModel;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.makeramen.roundedimageview.RoundedImageView;
 
 import org.w3c.dom.Text;
 
@@ -35,24 +43,31 @@ public class MainActivity extends AppCompatActivity implements OnProductClickLis
     private RecyclerView recyclerView;
     private Adapter adapter;
     private TextView username;
+    private ImageView logot;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        logot=findViewById(R.id.logout);
+        mAuth=FirebaseAuth.getInstance();
         username=findViewById(R.id.usernamee);
+        String name=mAuth.getCurrentUser().getEmail();
 
-        if(getIntent().hasExtra("user")){
-        String user=getIntent().getStringExtra("user");
-        username.setText( user);
-        }
+   /*     YoYo.with(Techniques.BounceInRight)
+                .duration(700)
+                .repeat(3)
+                .playOn(findViewById(R.id.usernamee));*/
+        username.setText(name);
+        username.setSelected(true);
 
-        recyclerView=findViewById(R.id.recycler);
-        adapter=new Adapter(getApplicationContext(),MainActivity.this);
+        recyclerView = findViewById(R.id.recycler);
+        adapter = new Adapter(getApplicationContext(), MainActivity.this);
         recyclerView.setAdapter(adapter);
 
-        productsViewModel=new ViewModelProvider(this).get(ProductsViewModel.class);
+        productsViewModel = new ViewModelProvider(this).get(ProductsViewModel.class);
 
         productsViewModel.getProducts();
 
@@ -63,11 +78,32 @@ public class MainActivity extends AppCompatActivity implements OnProductClickLis
             }
         });
 
+        logot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().getCurrentUser().delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+
+                    }
+                });
+            }
+        });
+
     }
 
     @Override
     public void onProductClick(int position) {
-       String title= adapter.getSelectedProduct(position).getTitle();
+
+        shoeItem(position);
+    }
+
+
+    private void shoeItem(int position){
+        String title= adapter.getSelectedProduct(position).getTitle();
         final Dialog dialog = new Dialog(MainActivity.this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setCancelable(true);
@@ -80,16 +116,13 @@ public class MainActivity extends AppCompatActivity implements OnProductClickLis
 
         dialog_title.setText(adapter.getSelectedProduct(position).getTitle());
         diaog_description.setText(adapter.getSelectedProduct(position).getDescription());
-        dialog_price.setText(String.valueOf(adapter.getSelectedProduct(position).getPrice())+"LE");
+        dialog_price.setText(adapter.getSelectedProduct(position).getPrice()+"LE");
 
         Glide.with(MainActivity.this)
                 .load(adapter.getSelectedProduct(position).getThumbnail())
                 .into(productImage);
         dialog.show();
-
-
-
-
-
     }
+
+
 }
